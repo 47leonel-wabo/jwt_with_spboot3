c041ccfea32b0f23135adc32ec5b5330bb806964
leonel47 <waboleonel@gmail.com>
2023-04-18 22:28:18 +0100

    Successfully connected to postgres db

---
+++ src/main/java/com/wbt/jwt_secure_app_spbt3/user/User.java
@@ -0,0 +1,39 @@
+package com.wbt.jwt_secure_app_spbt3.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "user_id_sequence", sequenceName = "user_id_sequence", allocationSize = 1)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
